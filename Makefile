b8f28827782b027cfe8771c719d78564d6eaa5ec
Rebecca Hanessian <Hanessian@MacBook-Pro.attlocal.net>
2022-05-22 19:49:17 -0500

    message

--- Makefile
+++ Makefile
@@ -3,10 +3,10 @@ CC = gcc
 all: master child
 
 master: master.c
-	gcc -o master master.c
+	gcc -std=c99 -o master master.c
 
 child: child.c
-	gcc -o child child.c
+	gcc -std=c99 -o child child.c
 
 clean:
 	$(RM) child master
--- child.c
+++ child.c
@@ -1,3 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/wait.h>  
+#include <unistd.h>    
+#include <string.h> 
+#include <signal.h>
+#include <time.h>
+#include <stdbool.h>
 #include "structure.h"
 
 struct shrd_mem *shm;
